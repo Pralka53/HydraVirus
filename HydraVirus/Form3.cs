7a19feaa252f54e1ef1e67b289a174490ec73877
Pralka53 <79445105+Pralka53@users.noreply.github.com>
2021-08-10 15:28:50 +0200

    Final version!
    Final version of hydra virus.

---
+++ HydraVirus/Form3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HydraVirus
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            this.Hide();
+            InitializeComponent();
+        }
+
+        private void Form3_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            Form2 frm2 = new Form2();
+            frm2.Show();
+            Form1 frm1 = new Form1();
+            frm1.Show();
+            Form4 frm4 = new Form4();
+            frm4.Show();
+        }
+    }
+}
---
+++ HydraVirus/TextFileMgr.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HydraVirus
+{
+    class TextFileMgr
+    {
+        public void TextMgr()
+        {
+            TextFileCreator TextFileCreator = new TextFileCreator();
+
+            TextFileCreator.Text1();
+            TextFileCreator.Text2();
+            TextFileCreator.Text3();
+            TextFileCreator.Text4();
+            TextFileCreator.Text5();
+            TextFileCreator.Text6();
+            TextFileCreator.Text7();
+            TextFileCreator.Text8();
+            TextFileCreator.Text9();
+            TextFileCreator.Text10();
+            TextFileCreator.Text11();
+            TextFileCreator.Text12();
+            TextFileCreator.Text13();
+            TextFileCreator.Text14();
+            TextFileCreator.Text15();
+            TextFileCreator.Text16();
+            TextFileCreator.Text17();
+            TextFileCreator.Text18();
+            TextFileCreator.Text19();
+            TextFileCreator.Text20();
+        }
+    }
+}
