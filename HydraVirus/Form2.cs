ad53075b34ff26afb885a1ef119703aec2a2be57
Pralka53 <79445105+Pralka53@users.noreply.github.com>
2021-12-03 17:06:23 +0100

    Add files via upload

---
+++ HydraVirus/Form2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HydraVirus
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            this.Hide();
+            InitializeComponent();
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Random random = new Random();
+
+            e.Cancel = true;
+            Form3 frm3 = new Form3();
+            frm3.StartPosition = FormStartPosition.Manual;
+            frm3.Location = new Point(random.Next(0, 1270), random.Next(0, 920));
+            frm3.Show();
+            Form1 frm1 = new Form1();
+            frm1.StartPosition = FormStartPosition.Manual;
+            frm1.Location = new Point(random.Next(0, 1270), random.Next(0, 920));
+            frm1.Show();
+            Form4 frm4 = new Form4();
+            frm4.StartPosition = FormStartPosition.Manual;
+            frm4.Location = new Point(random.Next(0, 1270), random.Next(0, 920));
+            frm4.Show();
+        }
+    }
+}
---
+++ HydraVirus/Form3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HydraVirus
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            this.Hide();
+            InitializeComponent();
+        }
+
+        private void Form3_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Random random = new Random();
+
+            e.Cancel = true;
+            Form2 frm2 = new Form2();
+            frm2.StartPosition = FormStartPosition.Manual;
+            frm2.Location = new Point(random.Next(0, 1270), random.Next(0, 920));
+            frm2.Show();
+            Form1 frm1 = new Form1();
+            frm1.StartPosition = FormStartPosition.Manual;
+            frm1.Location = new Point(random.Next(0, 1270), random.Next(0, 920));
+            frm1.Show();
+            Form4 frm4 = new Form4();
+            frm4.StartPosition = FormStartPosition.Manual;
+            frm4.Location = new Point(random.Next(0, 1270), random.Next(0, 920));
+            frm4.Show();
+        }
+    }
+}
